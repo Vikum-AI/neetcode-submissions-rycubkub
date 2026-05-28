e0f11a5b40846a69432468065bdc28bcccdaa8fb
Vikum Dabare <vikumdabare@gmail.com>
2026-05-28 13:15:26 +1000

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hash_map = defaultdict(int)
+
+        for c in s:
+            hash_map[c] += 1
+
+        for c in t:
+            hash_map[c] -= 1
+
+        for value in hash_map.values():
+            if value != 0:
+                return False
+
+        return True
